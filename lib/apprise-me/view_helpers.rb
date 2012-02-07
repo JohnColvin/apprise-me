89c0648d6ed3deedfd6deafbf46ed7b3ad432c29
John Colvin <colvin.68@osu.edu>
2012-02-07 16:53:28 -0500

    Excluded links with remote from apprise

--- lib/apprise-me/view_helpers.rb
+++ lib/apprise-me/view_helpers.rb
@@ -2,7 +2,7 @@ module AppriseMe
   module ViewHelpers
     def link_to(*args, &block)
       html_options = args[block_given? ? 1 : 2] || {}
-      html_options['data-apprise-confirm'] = html_options.delete(:confirm) if html_options[:confirm]
+      html_options['data-apprise-confirm'] = html_options.delete(:confirm) if html_options[:confirm] && !html_options[:remote]
       super *args, &block
     end
   end
